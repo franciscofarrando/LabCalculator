726f46a1dcab3320ea24d50e36652a16b5abbf7b
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-01 16:41:27 +0200

    module upload

---
+++ src/clases/Module.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Module {
+    public static int module(int a, int b){
+            return a%b;
+        }
+    public static double absolute(double a){
+        return (a>=0)?a:-a;
+        }
+}
---
+++ src/clases/UnitConverter.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class UnitConverter {
+    public static double CToF(double celsius){
+        return (celsius*9/5)+32;
+    }
+    public static double FToC(double fahrenheit){
+        return (fahrenheit-32) * 5/9;
+    }
+    public static double GToR(double grados){
+        return grados*3.14/180;
+    }
+    public static double RToG(double radianes){
+        return radianes*180/3.14;
+    }
+}
