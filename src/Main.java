8e3ce63a1b7d934acdee5fd56b6dc05758facdb1
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-01 16:02:47 +0200

    modulo y unidades

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        int module = Module.module(2,3);
+        System.out.println("Modulo: " + module);
+        double absolute = Module.absolute(-2.33);
+        System.out.println("Absolute: " + absolute);
+        double celsius = UnitConverter.FToC(23);
+        System.out.println(celsius);
+        double fahrenheit = UnitConverter.CToF(23);
+        System.out.println(fahrenheit);
+        double grados = UnitConverter.GToR(34);
+        System.out.println(grados);
+        double radianes = UnitConverter.RToG(56);
+        System.out.println(radianes);
 
-        System.out.println("Hello, World!");
     }
 }
