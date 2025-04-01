0f92a5a43d0e776a8c1c422c9a51f4144cd4a7f8
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-01 15:27:34 +0200

    metodos listos

---
+++ src/Module.java
@@ -0,0 +1,8 @@
+public class Module {
+    public static int module(int a, int b){
+            return a%b;
+        }
+    public static double absolute(double a){
+        return (a>=0)?a:-a;
+        }
+}
