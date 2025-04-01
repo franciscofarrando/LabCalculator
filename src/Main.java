97016e46f75bf23e83c97ac05ada59c78be09ba8
Saiguer10 <eljosemsp@gmail.com>
2025-04-01 16:02:18 +0200

    clase suma y resta

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int suma = Suma.sumar(2 , 3);
+        double sumaDecimales = Suma.sumar(2.5, 3.7);
+        double suma3Decimales = Suma.sumar(1.5, 0.3, 4.2);
+
+        int resta = Resta.restar(4 , 2);
+        double restaDecimales = Resta.restar(3.8, 1.5);
+        double resta3Decimales = Resta.restar(2.8, 1.9, 0.5);
+
+        System.out.println( suma);
+        System.out.println(sumaDecimales);
+        System.out.println(suma3Decimales);
+        System.out.println( resta);
+        System.out.println(restaDecimales);
+        System.out.println(resta3Decimales);
+
         System.out.println("Hello, World!");
     }
+
+
 }
---
+++ src/Resta.java
@@ -0,0 +1,26 @@
+public class Resta {
+
+    // restar dos numeros dobles reales
+
+    public static double restar ( double a, double b){
+        return a - b;
+    }
+
+    // restar dos numeros enteros
+
+    public static int restar ( int a, int b){
+        return a - b;
+    }
+
+    // restar tres numeros dobles reales
+
+    public static double restar ( double a, double b, double c){
+        return a - b - c;
+    }
+
+    //valor acumulado
+
+    public static double restarvalorAcumulado ( double valor){
+        return 0.0;
+    }
+}
